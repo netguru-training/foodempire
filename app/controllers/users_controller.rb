ee5cf9173420a31916edf53fa1749d956f2019d8
Przemek <sienkowskip@gmail.com>
2015-03-14 19:01:16 +0100

    modified users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,17 @@
 class UsersController < ApplicationController
+  expose(:user) { current_user }
+
   def show
   end
 
-  def edit
+  def update
+    flash[:notice] = 'User preferences have been updated' if user.update(user_params)
+    redirect_to user_path(user)
   end
 
-  def update
+  private
+
+  def user_params
+    params.require(:user).permit(:blacklisted_ingredients)
   end
 end
