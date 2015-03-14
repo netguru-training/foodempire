8ce04b53d1c16f7658a781e6d60108d8955c5dff
Przemek <sienkowskip@gmail.com>
2015-03-14 16:13:59 +0100

    updated schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150314144217) do
+ActiveRecord::Schema.define(version: 20150314151257) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "blacklisted_ingredients", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "ingredients", force: :cascade do |t|
     t.string   "name",       null: false
