ceb074b0bce04519a0ae3a61dfaf06763fe78483
Przemek <sienkowskip@gmail.com>
2015-03-15 11:08:02 +0100

    modified schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -16,11 +16,6 @@ ActiveRecord::Schema.define(version: 20150314155933) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "blacklisted_ingredients", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "favorites", force: :cascade do |t|
     t.integer  "recipe_id"
     t.integer  "user_id"
