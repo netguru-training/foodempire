3f0f90c2becd194a94c4b67576e7cc04b5dad33d
Przemek <sienkowskip@gmail.com>
2015-03-15 15:34:50 +0100

    modified associations

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,6 +1,8 @@
 class Ingredient < ActiveRecord::Base
   has_many :favorites, dependent: :destroy
-  has_and_belongs_to_many :recipes
+  #has_and_belongs_to_many :recipes
+  has_many :ingredients_recipes
+  has_many :recipes, through: :ingredients_recipes
   has_many :nutrition_values
   validates :name, presence: true
   validates :name, uniqueness: { case_sensitive: false }
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,7 +1,16 @@
 class Recipe < ActiveRecord::Base
   has_many :fevorites, dependent: :destroy
-  has_and_belongs_to_many :ingredients
-
+  #has_and_belongs_to_many :ingredients
+  has_many :ingredients_recipes
+  has_many :ingredients, through: :ingredients_recipes
   validates :name, :recipe_url, :picture_url, presence: true
   validates :name, uniqueness: { case_sensitive: false }
+
+  def self.without_blacklist(user)
+    return Recipe.all if user.blank?
+    Recipe.where.not(id: IngredientsRecipe
+      .select(:recipe_id)
+      .where(ingredient: user.blacklisted))
+  end
+
 end
